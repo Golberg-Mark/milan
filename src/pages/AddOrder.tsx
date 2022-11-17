6424ffe2a953af1be3e9271594afc9d2d8c3ac24
Mark_bitbucket <golbergmark@gmail.com>
2022-11-17 16:43:48 +0200

    Fixed route

--- src/pages/AddOrder.tsx
+++ src/pages/AddOrder.tsx
@@ -125,7 +125,7 @@ const AddOrder = () => {
   return (
     <AddOrderPage>
       <PageHeader>
-        <StyledLink to="/">
+        <StyledLink to="/dashboard">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g opacity="0.7">
               <path d="M9.57 5.92999L3.5 12L9.57 18.07" stroke="#111827" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
